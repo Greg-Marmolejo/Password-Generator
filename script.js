6ff866906f916b81469caf72815af02a819f732b
Greg Marmolejo <greg_marmolejo@yahoo.com>
2019-12-13 20:22:19 -0600

    added password function and copy to clipboard function to js file.

--- script.js
+++ script.js
@@ -18,8 +18,8 @@ generateEl.addEventListener("click", function () {
     //Create an empty array, and create different arrays for the types of characters to be used.
     var password = ""
     var requiredLength = 0
-    var charList = new Array(pwLength);
-    var required = new Array(requiredLength);
+    var charList = [];
+    var required = [];
     var charListUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
     var charListLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     var charListNumber = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
@@ -32,7 +32,7 @@ generateEl.addEventListener("click", function () {
 
     // if yes, then add special characters arrray to the empty array, randomly generate a special character, and put it in a "required" array.  
     if (confirmSpecial) {
-        charList.push(charListSpecial);
+        charList = charList.concat(charListSpecial);
         var specChar = charListSpecial[Math.floor(Math.random() * 10)];
         required.push(specChar);
         requiredLength = requiredLength + 1;
@@ -43,7 +43,7 @@ var confirmNumber = confirm("Would you like to use numeric characters?");
 
 // if yes, then add number arrray to the empty array, randomly generate a number, and put it in a "required" array.
 if(confirmNumber) {
-    charList.push(charListNumber);
+    charList = charList.concat(charListNumber);
     var numChar = charListNumber[Math.floor(Math.random() * 10)];
     required.push(numChar);
     requiredLength = requiredLength +1;
@@ -54,7 +54,7 @@ var confirmLower = confirm("Would you like to use lowercase letters?");
 
 // if yes, then add lowercase arrray to the empty array, randomly generate a lowercase letter, and put it in a "required" array.
 if(confirmLower) {
-    charList.push(charListLower);
+    charList = charList.concat(charListLower);
     var lowChar = charListLower[Math.floor(Math.random() * 10)];
     required.push(lowChar);
     requiredLength = requiredLength +1;
@@ -65,21 +65,49 @@ var confirmUpper = confirm("Would you like to use uppercase letters?");
 
 // if yes, then add uppercase arrray to the empty array, randomly generate a uppercase letter, and put it in a "required" array.
 if(confirmUpper) {
-    charList.push(charListUpper);
+    charList = charList.concat(charListUpper);
     var upChar = charListUpper[Math.floor(Math.random() * 10)];
     required.push(upChar);
     requiredLength = requiredLength +1;
 }
 
-// We now have the array for the password generator.  
+// Validate that at least one character type was selected.
+if (requiredLength === 0) {
+    alert("Please choose at least one character type.");
+    return
+}
+
+generatePassword();
+
+// // We now have the array for the password generator.  
 function generatePassword() {
-    
+
+    var passwordArray =[]
     for (var i = 0; i < (pwLength-requiredLength); i++) {
-        password += charList[Math.floor(Math.random() * charList.length)];
+        passwordChar = charList[Math.floor(Math.random() * charList.length)];
+        passwordArray.push(passwordChar);
     }
+
+    // Smash requiredArray and passwordArray together
+    passwordArray = required.concat(passwordArray);
+
+    // Convert passwordArray to a string.  This will be the password
+    password = passwordArray.join("");
+
+
     document.getElementById('passwordBox').value = password;
-    }
-    console.log(required);
-    console.log(charList);
+}
 
 });
+
+// //The copy button copies the password to clipboard and writes password into the text area labelled "Your Secure Password".
+var copyEl = document.querySelector("#copy");
+copyEl.addEventListener("click", function() {
+
+    var copyText = document.getElementById("passwordBox");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+    
+});
+
